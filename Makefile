ad41225022ff1f013ec6d0d9be4b61706dadb393
Funky Gao <funky.gao@gmail.com>
2017-04-21 09:48:40 +0800

    play benchmark

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 present:
-	@present 
+	@present&
+	open http://127.0.0.1:3999/dbus.slide
 
 doc:
 	godoc golang.org/x/tools/present | less
--- play/benchmark_chan.go
+++ play/benchmark_chan.go
@@ -1,32 +1,33 @@
+// +build OMIT
+
 package main
 
 import (
 	"fmt"
 	"strings"
 	"testing"
-
-	"github.com/funkygao/gobench/util"
 )
 
-var buf int
+var chanSize int
 
 func main() {
-	bs := []int{0, 1, 2, 5, 10, 50, 100, 200, 500, 1000, 5000}
-	fmt.Printf("%5s %s\n", "qlen", "with int")
-	for _, s := range bs {
-		buf = s
-		fmt.Printf("%5d %s\n", buf, testing.Benchmark(benchmarkChanInt).String())
+	chanSizes := []int{0, 1, 50, 100, 500, 1000, 5000}
+	fmt.Println("benchmark channel of int")
+	fmt.Println("chan buf size")
+	for _, sz := range chanSizes {
+		chanSize = sz
+		fmt.Printf("%13d %s\n", chanSize, testing.Benchmark(benchmarkChanInt).String())
 	}
-	fmt.Printf("%5s %s\n", "qlen", "with [100]byte")
-	for _, s := range bs {
-		buf = s
-		fmt.Printf("%5d %s\n", buf, testing.Benchmark(benchmarkChanByte100).String())
+	fmt.Println("benchmark channel of [100]byte")
+	fmt.Println("chan buf size")
+	for _, s := range chanSizes {
+		chanSize = s
+		fmt.Printf("%13d %s\n", chanSize, testing.Benchmark(benchmarkChanByte100).String())
 	}
 }
 
-// START OMIT
 func benchmarkChanInt(b *testing.B) {
-	ch := make(chan int, buf)
+	ch := make(chan int, chanSize)
 	go func() {
 		for i := 0; i < b.N; i++ {
 			ch <- i
@@ -37,12 +38,10 @@ func benchmarkChanInt(b *testing.B) {
 
 	for _ = range ch {
 	}
-}
-
-// END OMIT
+} //
 
 func benchmarkChanByte100(b *testing.B) {
-	ch := make(chan []byte, buf)
+	ch := make(chan []byte, chanSize)
 	data := []byte(strings.Repeat("x", 100))
 	go func() {
 		for i := 0; i < b.N; i++ {
